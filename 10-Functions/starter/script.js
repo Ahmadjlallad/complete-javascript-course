05f48ea35f871e73be7c51370dc32d3db22abca7
Ahmad Jallad <ahmadjlad@hotmail.com>
2021-08-02 15:58:10 +0300

    done was a fun section

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,12 @@
-'use strict';
+// 'use strict';
+// const greet = greeting => {
+//   return name => console.log(`${greeting} ${name}`);
+// };
+// const greeterHi = greet('سلام عليكم');
+// greeterHi(`Ahmad`);
+// // ===========>>>>>#TODO#<<<<<<===========
+// const addTax = rate => value => value + value * rate;
+// const addVAT = addTax(0.23);
+// console.log(addVAT(100));
+// console.log(addVAT(25));
+// console.log(addVAT(180));
